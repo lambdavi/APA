4b02fc7bf2173e7a92589d4c3bfe0997939dc99a
DavideB <37984186+DavideBu@users.noreply.github.com>
2020-09-30 14:13:53 +0200

    Update main.c
    Aggiunti i commenti

--- main.c
+++ main.c
@@ -9,10 +9,12 @@ char *rimpiazza(char *str, char *strVecchia, char *strNuova)
 {
     static char buff[200];
     char *p;
+    //Imposto un puntatore dove trovo la corrispondenza
     if(!(p = strstr(str, strVecchia)))
         return str;
+    //copio in buff la parte prima di quella che deve essere sostituita
     strncpy(buff, str, p-str);
-    buff[p-str] = ' ';
+    //inizio a scrivere su buff la nuova stringa a poi il resto della vecchia stringa 
     sprintf(buff+(p-str), "%s%s", strNuova, p+strlen(strVecchia));
     return buff;
 }
@@ -24,14 +26,17 @@ int main() {
     f1=fopen("sorgente.txt","r+");
     f2=fopen("dizionario.txt","r+");
     f3=fopen("ricondizionato.txt","w");
+    
     if(f1==NULL){printf("\nErrore nell'apertura del file 1\n"); exit(EXIT_FAILURE);}
     if(f2==NULL){printf("\nErrore nell'apertura del file 2\n"); exit(EXIT_FAILURE);}
+    
     fscanf(f2,"%d",&S);
     dict dizionario[S];
+    //Leggo il file dizionario e salvo nella struct
     for(int i=0;i<S;i++){
         fscanf(f2,"%s %s",dizionario[i].ricod, dizionario[i].orig);
     }
-
+    //Leggo una riga alla volta del file sorgente e sostituisco le parole
     while(fgets(str,200,f1)!=NULL){
         strcpy(str2,rimpiazza(str,dizionario[0].orig,dizionario[0].ricod));
         for (int i = 1; i < S; ++i) {
